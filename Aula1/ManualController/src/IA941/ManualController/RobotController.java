f80337eefa2cac04a42de6c418fff8cd4a38968b
Fabio Grassiotto <fabiogr@gmail.com>
2018-03-04 10:47:51 -0300

    Working code, add gitignore

--- Aula1/ManualController/src/IA941/ManualController/RobotController.java
+++ Aula1/ManualController/src/IA941/ManualController/RobotController.java
@@ -10,6 +10,9 @@ import ws3dproxy.WS3DProxy;
 import ws3dproxy.model.Creature;
 import ws3dproxy.model.World;
 import ws3dproxy.model.WorldPoint;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  *
@@ -17,13 +20,29 @@ import ws3dproxy.model.WorldPoint;
  */
 public class RobotController {
     
+        // Movement and Rotation constants
         private static final double MOVE_SPEED = 2.0f;
-        private static final double TURN_SPEED = 90.0f;
-        private static final int THREAD_SLEEP = 300;
+        private static final double ROTATION_SPEED = 4.0f;
+        private static final long STOP_MOVEMENT_DELAY = 300;
+        private static final long STOP_ROTATION_DELAY = 1000;
         
+        private final ScheduledExecutorService mScheduler = Executors.newScheduledThreadPool(1);
+        
+        // World handles
         private Creature mCreature;
         private WorldPoint mPosition;
     
+        // Runnable to stop the creature movement, out of thread.
+        final Runnable stopMovement = new Runnable() {
+            public void run() {
+                try{
+                    mCreature.stop();
+                } catch (CommandExecException e) {
+                    System.out.println("Command error");
+                }
+            }
+        };
+                   
         RobotController() {
         }
         
@@ -38,10 +57,7 @@ public class RobotController {
                 World.createFood(0, 250, 210);
                 mCreature = proxy.createCreature(100,450,0);
                 mCreature.start();
-                mPosition = mCreature.getPosition();
-                double pitch = mCreature.getPitch();
-                double fuel = mCreature.getFuel();
-                //c.moveto(1, 200, 200);            
+                mPosition = mCreature.getPosition();    
             } catch (CommandExecException e) {
                 System.out.println("Erro capturado"); 
             }
@@ -49,61 +65,51 @@ public class RobotController {
         
         public void moveForward() {
             try{
-                
-            mCreature.start();
-            mCreature.move(MOVE_SPEED, MOVE_SPEED, 0);
-            Thread.sleep(THREAD_SLEEP);
-            mCreature.stop();
-            
+                mCreature.start();
+                mCreature.move(MOVE_SPEED, MOVE_SPEED, 0);
+
+                // Stop movement after a delay to avoid blocking the UI thread.
+                mScheduler.schedule(stopMovement, STOP_MOVEMENT_DELAY, TimeUnit.MILLISECONDS);
             } catch (CommandExecException e) {
-                System.out.println("Erro capturado"); 
-            } catch (InterruptedException ex) {
-                System.out.println("Erro capturado"); 
-            }
+                System.out.println("Command error"); 
+            } 
         }
         
         public void moveBackwards() {
             try{
-                
-            mCreature.start();
-            mCreature.move(-MOVE_SPEED, -MOVE_SPEED, 0);
-            Thread.sleep(THREAD_SLEEP);
-            mCreature.stop();
-            
+                mCreature.start();
+                mCreature.move(-MOVE_SPEED, -MOVE_SPEED, 0);
+
+                // Stop movement after a delay to avoid blocking the UI thread.
+                mScheduler.schedule(stopMovement, STOP_MOVEMENT_DELAY, TimeUnit.MILLISECONDS);
             } catch (CommandExecException e) {
-                System.out.println("Erro capturado"); 
-            } catch (InterruptedException ex) {
-                System.out.println("Erro capturado"); 
-            }
+                System.out.println("Command error"); 
+            } 
         }
         
         public void turnRight() {
             try{
+                mCreature.start();
+                mCreature.rotate(ROTATION_SPEED);
                 
-            mCreature.start();
-            mCreature.move(0, 0, Math.toRadians(TURN_SPEED));
-            Thread.sleep(THREAD_SLEEP);
-            mCreature.stop();
-            
+                // Stop rotation after a delay to avoid blocking the UI thread.
+                mScheduler.schedule(stopMovement, STOP_ROTATION_DELAY, TimeUnit.MILLISECONDS);
             } catch (CommandExecException e) {
-                System.out.println("Erro capturado"); 
-            } catch (InterruptedException ex) {
-                System.out.println("Erro capturado"); 
-            }
+                System.out.println("Command error"); 
+            } 
         }
         
         public void turnLeft() {
             try{
+                mCreature.start();
+                mCreature.rotate(-ROTATION_SPEED);
                 
-            mCreature.start();
-            mCreature.move(0, 0, Math.toRadians(TURN_SPEED));
-            Thread.sleep(THREAD_SLEEP);
-            mCreature.stop();
-            
+                // Stop rotation after a delay to avoid blocking the UI thread.
+                mScheduler.schedule(stopMovement, STOP_ROTATION_DELAY, TimeUnit.MILLISECONDS);
             } catch (CommandExecException e) {
-                System.out.println("Erro capturado"); 
-            } catch (InterruptedException ex) {
-                System.out.println("Erro capturado"); 
-            }
+                System.out.println("Command error"); 
+            } 
+            
         }
+
 }
