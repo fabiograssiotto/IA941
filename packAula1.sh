0bbd297521d445f7f714b72a191d7d002d3179cc
Fabio Grassiotto <fabiogr@gmail.com>
2018-03-06 21:12:50 -0300

    Remove ignored files

--- packAula1.sh
+++ packAula1.sh
@@ -1,5 +1,6 @@
+mkdir -p Aula1-dist/src/ManualController
 mkdir -p Aula1-dist/ManualController
 mkdir -p Aula1-dist/ws3d
+cp -r Aula1/ManualController/* Aula1-dist/src/ManualController
 cp -r Aula1/ManualController/dist/* Aula1-dist/ManualController
-cp -r Aula1/ws3d/dist/* Aula1-dist/ws3d
-tar -czvf Aula1-dist.tar.gz Aula1-dist  
+cp -r Aula1/ws3d/dist/* Aula1-dist/ws3d  
