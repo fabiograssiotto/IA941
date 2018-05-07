6e5b8247757cbf8ebcc6f3b1ffe0b12b3675fa4f
Fabio Grassiotto <fabiogr@gmail.com>
2018-05-07 12:42:25 -0300

    Update

--- Aula6/DemoJSOAR/src/Simulation/Main.java
+++ Aula6/DemoJSOAR/src/Simulation/Main.java
@@ -34,7 +34,7 @@ public class Main
 
             //Start enviroment data
             Environment e = new Environment(Boolean.FALSE);
-            SoarBridge soarBridge = new SoarBridge(e,soarRulesPath,false);
+            SoarBridge soarBridge = new SoarBridge(e,soarRulesPath,true);
             MindView mv = new MindView(soarBridge);
             mv.setVisible(true);
 
---
+++ Aula9/run-Aula9.bat
@@ -0,0 +1,2 @@
+# Script to launch WordServer3d
+start java -jar ws3d/WorldServer3D.jar
