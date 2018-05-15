f469eb2263eed138470dffdc2c6e6bc256221773
Fabio Grassiotto <fabiogr@gmail.com>
2018-05-15 19:08:45 -0300

    Update run-Aula9.sh

--- Aula9/run-Aula9.sh
+++ Aula9/run-Aula9.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # Script to launch WordServer3d
 java -jar ws3d/WorldServer3D.jar &
+sleep 5
+# Script to launch ClarionApp
+mono DemoClarion\ClarionApp\bin\Release\ClarionApp.exe &
+# Script to launch DemoSoar
+java -jar DemoJSOAR\DemoJSOAR.jar &
