5218b5d6be5d9876596d3a76312a80aab77cf832
Fabio Grassiotto <fabiogr@gmail.com>
2018-05-15 11:51:17 -0300

    Add files via upload

--- Aula9/run-Aula9.bat
+++ Aula9/run-Aula9.bat
@@ -1,2 +1,5 @@
-# Script to launch WordServer3d
+rem Script to launch WordServer3d
 start java -jar ws3d/WorldServer3D.jar
+sleep 5
+rem Script to launch ClarionApp
+start mono DemoClarion\ClarionApp\bin\Debug\ClarionApp.exe
